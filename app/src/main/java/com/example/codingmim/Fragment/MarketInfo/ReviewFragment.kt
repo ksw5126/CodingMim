88753082354c127c0363be62fc51228dc80c3fc4
ksw5126 <ksw3149@naver.com>
2020-05-21 12:40:58 +0900

    review list

--- app/src/main/java/com/example/codingmim/Fragment/MarketInfo/ReviewFragment.kt
+++ app/src/main/java/com/example/codingmim/Fragment/MarketInfo/ReviewFragment.kt
@@ -1,24 +1,74 @@
 package com.example.codingmim.Fragment.MarketInfo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.example.codingmim.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_review.view.*
+import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
  */
 class ReviewFragment : Fragment() {
 
+    private lateinit var auth : FirebaseAuth
+
+    private val db = FirebaseFirestore.getInstance()
+
+    private val text_array = ArrayList<String>()
+    private val nickname_array = ArrayList<String>()
+    private val rating_array = ArrayList<String>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review, container, false)
+
+        auth = FirebaseAuth.getInstance()
+
+        val view : View = inflater.inflate(R.layout.fragment_review, container, false)
+
+        // adapter 연결
+        val review_adapter = ReviewListAdapter(requireContext(), nickname_array, text_array, rating_array)
+        view.listview_review.adapter = review_adapter
+
+        // db
+        db.collection("reviews")
+            .get()
+            .addOnSuccessListener { result ->
+                for(document in result) {
+                    nickname_array.add(document.get("writer") as String)
+                    text_array.add(document.get("text") as String)
+                    rating_array.add(document.get("rating") as String)
+                }
+
+                review_adapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+
+            }
+
+        view.write_button.setOnClickListener {
+
+            if(auth.currentUser == null) {
+                Toast.makeText(requireContext(), "login please", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(requireContext(), WriteActivity::class.java)
+                startActivity(intent)
+            }
+
+
+        }
+
+        return view
     }
 
 }
---
+++ app/src/main/java/com/example/codingmim/Fragment/MarketInfo/WriteActivity.kt
@@ -0,0 +1,67 @@
+package com.example.codingmim.Fragment.MarketInfo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.codingmim.MainActivity
+import com.example.codingmim.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_my_comin.*
+import kotlinx.android.synthetic.main.activity_write.*
+
+class WriteActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var nickname : String
+    private lateinit var rating_num : String
+
+    private val db = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+
+        auth = FirebaseAuth.getInstance()
+
+        // rating
+        rating_area.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            rating_num = rating.toString()
+        }
+
+        // 닉네임 받아오기
+        val docRef = db.collection("users").document(auth.currentUser?.uid.toString())
+        docRef.get().addOnSuccessListener { documentSnapshot ->
+
+            nickname = documentSnapshot.get("nickname") as String
+
+        }
+
+
+        // db로 보내버리기~
+        writing_button.setOnClickListener {
+
+            val form = hashMapOf(
+                "text" to text_input_area.text.toString(),
+                "writer" to nickname,
+                "rating" to rating_num
+            )
+
+            db.collection("reviews")
+                .add(form)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    finish()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                }
+
+        }
+
+    }
+}
